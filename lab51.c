2fa653eeab40183b4d63266c424a77877cef2de6
chautuanminh <153380747+chautuanminh@users.noreply.github.com>
2024-05-20 22:16:57 +0700

    Update lab51.c

--- lab51.c
+++ lab51.c
@@ -13,17 +13,18 @@ int main () {
                 printf("what is your %d number: ", i+1);
                 scanf("%d", &A[i]);
             }
-            for (i = 0; i <n ; i++) {
-                  if (A[i] <= A[i+1]) {
-                    max = A[i+1];
-                  }else {
-                      max=A[i];
-                  }
-            }
+            max = A[0];
+
+    
+    for (i = 1; i < n; i++) {
+        if (A[i] > max) {
+            max = A[i];
+        }
+    }
+
             printf("%d is the maximum value of the set", max);
         return 0;
         }
         
      
 }
-	
