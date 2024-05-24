c8c78b95209281266c5b0a4ee6b599051ece2cfc
chautuanminh <153380747+chautuanminh@users.noreply.github.com>
2024-05-24 11:35:22 +0700

    Update fibonacci.c

--- fibonacci.c
+++ fibonacci.c
@@ -6,10 +6,11 @@ int main () {
     A[1] = 1;
     printf("how many number of the fibonacci sequence: " );
     scanf("%d",&n);
-    if (n<= 0) {
-        printf("error please re-enter: ");
+    while (n <= 0 || n > 1000) {
+        printf("Error: Please enter a positive number less than or equal to 1000: ");
         scanf("%d", &n);
     }
+
     for (i = 0;i<n;i++) {
            A[i+2] = A[i+1] + A[i];
     }
