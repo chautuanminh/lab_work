def2d06b64e29bbb79c84931d60eb4523c64fdb6
chautuanminh <153380747+chautuanminh@users.noreply.github.com>
2024-05-25 19:29:54 +0700

    Update bubble_sort.c

--- bubble_sort.c
+++ bubble_sort.c
@@ -33,3 +33,30 @@ int main() {
     
 
 }
+//anotherway
+#include <stdio.h>
+int main () {
+    int i, k,A[1000], j, n;
+    printf("how many num:");
+    scanf("%d", &n);
+    
+    for (i=0;i<n;i++) {
+        printf("what is the %d number: ", i+1);
+        scanf("%d", &A[i]);
+    }
+    for (i=0;i<n-1;i++){ //i<n-1
+        for (j=0;j<n-1-i;j++) {   //j< n-1-i
+            if (A[j]>A[j+1]) {
+                k = A[j];
+                A[j] = A[j+1];
+                A[j+1] = k;
+            }
+        }
+    }
+    for (i=0;i<n;i++) {
+        printf("%d\n", A[i]);
+    }
+    return 0;
+
+}
+
