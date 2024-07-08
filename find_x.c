c68088f3e6a5a9a32b78abab7fc33058e2db9d4a
chautuanminh <153380747+chautuanminh@users.noreply.github.com>
2024-07-08 21:41:10 +0700

    Create find_x.c

---
+++ find_x.c
@@ -0,0 +1,24 @@
+#include <stdio.h> 
+#include <string.h> 
+
+int main () {
+    int i, k;
+    k = 0;
+    char A[200], B[200], x;
+
+    printf("what is da ting: ");
+    fgets(A, sizeof(A), stdin);
+
+    printf("what is DA TING: ");
+    scanf("%c", &x);
+    
+    for (i = 0; i<strlen(A); i++) {
+        if(A[i] == x) {
+            B[k++] = A[i];
+            printf("The letter 'x' is in position %d\n", i+1);
+        }
+    }
+
+    printf("the number of the letter 'x entered is %d", strlen(B));
+    return 0;
+}
